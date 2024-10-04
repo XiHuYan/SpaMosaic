606aa7e61807b3f8fb5321256029809c5906d189
XiHuYan <41266664+XiHuYan@users.noreply.github.com>
2024-10-04 17:43:51 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,9 +21,8 @@ extensions = [
 ]
 nbsphinx_execute = 'never'
 templates_path = ['_templates']
-exclude_patterns = []
 
-
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
