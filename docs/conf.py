b10c47b11154c0af8704d1e26acbf2a8d50d1da7
XiHuYan <41266664+XiHuYan@users.noreply.github.com>
2024-10-04 11:53:21 +0800

    change the extension

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = 'v1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark','sphinx_markdown_tables']
+extensions = [] # ['recommonmark','sphinx_markdown_tables']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,6 +24,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
+# html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
